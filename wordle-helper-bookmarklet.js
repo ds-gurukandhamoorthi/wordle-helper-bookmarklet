433775fa76dea80dc184c330db90b8d237b90926
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2022-02-16 22:31:04 +0100

    Add positional constraints: present-at-index, present-but-not-at-index

--- wordle-helper-bookmarklet.js
+++ wordle-helper-bookmarklet.js
@@ -6,4 +6,17 @@ allowed=[...alphs].filter(c=>lettersFeedback[c]!='absent').join('');
 cmd+=`|grep -v '[^${allowed}]'`
 mustBePresent=[...alphs].filter(c=>lettersFeedback[c]=='present').map(c=>`/${c}/`).join(' && ');
 cmd+=`|awk '${mustBePresent}'`
+let positionalConstraints=[];
+let forceCharAt=(ch, ind) => '/' + '.'.repeat(ind)+ch+'.'.repeat(5-ind-1) + '/';
+for(let i=0; i<6; i++){
+    feedback=document.getElementsByTagName('game-app')[0]['$board'].childNodes[i]._evaluation;
+    letrs=document.getElementsByTagName('game-app')[0]['$board'].childNodes[i]._letters;
+    for (let j=0; j<letrs.length; j++){
+        if (feedback[j] == 'present'){
+            positionalConstraints.push('!'+ forceCharAt(letrs.charAt(j), j)); //not at this place
+        }else if(feedback[j] == 'correct'){
+            positionalConstraints.push(forceCharAt(letrs.charAt(j), j)); //at this place
+        }
+    }
+}
 prompt('Copy paste in bash prompt',cmd);

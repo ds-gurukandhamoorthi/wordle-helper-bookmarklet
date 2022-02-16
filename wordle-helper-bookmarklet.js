08db1ceaa109654ddb7e51d004a6913049aeafcd
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2022-02-16 11:50:01 +0100

    Sample only from the allowed-chars-pool

--- wordle-helper-bookmarklet.js
+++ wordle-helper-bookmarklet.js
@@ -1,3 +1,7 @@
 cmd='cat /usr/share/dict/american-english';
 cmd+="|awk 'length($0) == 5'";
-prompt('Copy past in bash prompt',cmd);
+lettersfeedback=document.getElementsByTagName('game-app')[0]["$keyboard"]['_letterEvaluations'];
+alphs='abcdefghijklmnopqrstuvwxyz';
+allowed=[...alphs].filter(c=>lettersfeedback[c]!='absent').join('');
+cmd+=`|grep -v '[^${allowed}]'`
+prompt('Copy paste in bash prompt',cmd);

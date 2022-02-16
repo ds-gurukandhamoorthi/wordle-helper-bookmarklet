b7707c10544019724039e5ff2750427f7c5324f4
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2022-02-16 19:26:16 +0100

    Constrain some letters to be present

--- wordle-helper-bookmarklet.js
+++ wordle-helper-bookmarklet.js
@@ -1,7 +1,9 @@
 cmd='cat /usr/share/dict/american-english';
 cmd+="|awk 'length($0) == 5'";
-lettersfeedback=document.getElementsByTagName('game-app')[0]["$keyboard"]['_letterEvaluations'];
+lettersFeedback=document.getElementsByTagName('game-app')[0]["$keyboard"]['_letterEvaluations'];
 alphs='abcdefghijklmnopqrstuvwxyz';
-allowed=[...alphs].filter(c=>lettersfeedback[c]!='absent').join('');
+allowed=[...alphs].filter(c=>lettersFeedback[c]!='absent').join('');
 cmd+=`|grep -v '[^${allowed}]'`
+mustBePresent=[...alphs].filter(c=>lettersFeedback[c]=='present').map(c=>`/${c}/`).join(' && ');
+cmd+=`|awk '${mustBePresent}'`
 prompt('Copy paste in bash prompt',cmd);

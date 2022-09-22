7b97df8c9d3010dea53151a22a0899e44f0dafae
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2022-09-22 23:58:25 +0200

    Fix: Incorporate changes made in the website

--- wordle-helper-bookmarklet.js
+++ wordle-helper-bookmarklet.js
@@ -1,24 +1,29 @@
-cmd='cat /usr/share/dict/american-english';
+let cmd='cat /usr/share/dict/american-english';
 cmd+="|awk 'length($0) == 5'";
-lettersFeedback=document.getElementsByTagName('game-app')[0]["$keyboard"]['_letterEvaluations'];
-alphs='abcdefghijklmnopqrstuvwxyz';
-allowed=[...alphs].filter(c=>lettersFeedback[c]!='absent').join('');
+const alphs='abcdefghijklmnopqrstuvwxyz';
+let disallowedKeys=document.querySelectorAll('[data-state="absent"][data-key]');
+let disallowed=[...disallowedKeys].map(k=>k.getAttribute('data-key'));
+let allowed=[...alphs].filter(c=>!disallowed.includes(c)).join('');
 cmd+=`|grep -v '[^${allowed}]'`
-mustBePresent=[...alphs].filter(c=>lettersFeedback[c]=='present').map(c=>`/${c}/`).join(' && ');
+let mustBePresentKeys=document.querySelectorAll('[data-state="present"][data-key],[data-state="correct"][data-key]');
+let mustBePresent=[...mustBePresentKeys].map(k=>k.getAttribute('data-key')).map(c=>`/${c}/`).join(' && ');
 if(mustBePresent.length > 0){
     cmd+=`|awk '${mustBePresent}'`
 }
 let positionalConstraints=[];
 let forceCharAt=(ch, ind) => '/' + '.'.repeat(ind)+ch+'.'.repeat(5-ind-1) + '/';
-for(let i=0; i<6; i++){
-    feedback=document.getElementsByTagName('game-app')[0]['$board'].childNodes[i]._evaluation;
-    letrs=document.getElementsByTagName('game-app')[0]['$board'].childNodes[i]._letters;
-    for (let j=0; j<letrs.length; j++){
-        if (feedback[j] == 'present'){
-            positionalConstraints.push('!'+ forceCharAt(letrs.charAt(j), j)); //not at this place
-        }else if(feedback[j] == 'correct'){
-            positionalConstraints.push(forceCharAt(letrs.charAt(j), j)); //at this place
-        }
+const tiles=document.querySelectorAll('div[data-state]');
+for(let i=0; i<6; i++){//rows: each attempt
+    const WORD_LEN=5;
+    for (let j=0; j<WORD_LEN; j++){//columns: each character
+            let currTile = tiles[i*WORD_LEN + j];
+            if (currTile.getAttribute('data-state') == 'present'){
+                //present but must not be in this position (at this index)
+                positionalConstraints.push('!'+ forceCharAt(currTile.textContent, j));
+            }else if (currTile.getAttribute('data-state') == 'correct'){
+                //must be present at this index
+                positionalConstraints.push(forceCharAt(currTile.textContent, j));
+            }
     }
 }
 if(positionalConstraints.length > 0){
